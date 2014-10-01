eba0cfb55c5b0250a4c69afba014027d2832bbb2
Henry Stanley <henry@henrystanley.com>
2014-10-01 23:38:12 +0100

    Switched away from Watir to Mechanize

--- server.rb
+++ server.rb
@@ -1,36 +1,30 @@
 require 'sinatra'
-require 'watir-webdriver'
+require 'mechanize'
 require 'open-uri'
 require 'nokogiri'
+
+get '/' do
+  erb :index
+end
 
 get '/:tracking_number' do |tracking_number|
   @yield = get_tracking_status(tracking_number)
   erb :track
 end
 
-
 def get_tracking_page(tracking_number)
-  b = Watir::Browser.new :phantomjs
-  b.goto 'http://www.tnt.com/webtracker/tracker.do?navigation=1&respLang=en&respCountry=au'
-  b.textarea(:name => "cons").set "#{tracking_number}"
-  b.button(:name => "btnX").click 
-  html = b.html
-  b.close
-  html
+  agent = Mechanize.new
+  agent.get('http://www.tnt.com/webtracker/tracker.do?navigation=1&respLang=en&respCountry=au')
+  agent.page.forms[1]["cons"] = "#{tracking_number}" # enters the tracking number in the second form on the page
+  agent.page.forms[1].submit # submits the form
+  agent.page # returns the full HTML of the resulting page
 end
 
 def get_tracking_status(tracking_number)
   # get page HTML
-  page = get_tracking_page(tracking_number)
-  html = Nokogiri::HTML(page)
-  # @tracking_table = html.css('table')[1]
-  # @package_details = html.search("//table")[4] 
+  html = get_tracking_page(tracking_number)
   html.search('//table[@width="470"]').remove
-  # html.search('//table[@width="510"]').remove
   results = html.search('//table')
   results.search('//table[position() < 6]', '//button', '//td[1]').remove
-  # results = results.search('//button').remove
   results
 end
-
-# get_tracking_status(922648777)
