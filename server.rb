f82b5447aef822fee3ab54f906e443246ba2580b
Henry Stanley <henry@henrystanley.com>
2014-10-01 18:04:33 +0100

    Switched from watir to phantomjs

--- server.rb
+++ server.rb
@@ -10,7 +10,8 @@ end
 
 
 def get_tracking_page(tracking_number)
-  b = Watir::Browser.start 'http://www.tnt.com/webtracker/tracker.do?navigation=1&respLang=en&respCountry=au'
+  b = Watir::Browser.new :phantomjs
+  b.start 'http://www.tnt.com/webtracker/tracker.do?navigation=1&respLang=en&respCountry=au'
   b.textarea(:name => "cons").set "#{tracking_number}"
   b.button(:name => "btnX").click 
   html = b.html
