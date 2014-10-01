b3e509120b317e762a7fe2e430a41ee2a8a8237c
Henry Stanley <henry@henrystanley.com>
2014-10-01 17:33:21 +0100

    Server ready to go - extracts page content from TNT and presents it. Using Bootstrap

--- server.rb
+++ server.rb
@@ -1,8 +1,33 @@
+require 'sinatra'
 require 'watir-webdriver'
+require 'Nokogiri'
+
+get '/' do
+  @yield = get_tracking_status(922648777)
+  erb :track
+end
+
 
 def get_tracking_page(tracking_number)
   b = Watir::Browser.start 'http://www.tnt.com/webtracker/tracker.do?navigation=1&respLang=en&respCountry=au'
   b.textarea(:name => "cons").set "#{tracking_number}"
   b.button(:name => "btnX").click 
-  b.html
+  html = b.html
+  b.close
+  html
 end
+
+def get_tracking_status(tracking_number)
+  # get page HTML
+  page = get_tracking_page(tracking_number)
+  html = Nokogiri::HTML(page)
+  # @tracking_table = html.css('table')[1]
+  # @package_details = html.search("//table")[4] 
+  html.search('//table[@width="470"]').remove
+  # html.search('//table[@width="510"]').remove
+  results = html.search('//table')
+  results.search('//table[position() < 6]').remove
+  results
+end
+
+# get_tracking_status(922648777)
